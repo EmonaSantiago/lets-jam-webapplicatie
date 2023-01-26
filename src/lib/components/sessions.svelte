81c2f782a7f879d1e7cc09c5be387af8d54f8d7a
Emona Santiago <emonasantiago1207@gmail.com>
2023-01-26 18:45:47 +0100

    css

--- src/lib/components/sessions.svelte
+++ src/lib/components/sessions.svelte
@@ -1,10 +1,52 @@
 <section id="sessions">
-  <h4>Sessions content</h4>
+  <div class="container">
+    <h2>We web Navigeren op het web & de kracht van Css</h2>
+    <h3>Cyd Stumpel</h3>
+    <p>
+      2022-06-21T14:51:22.000Z <br />
+      Een talk over de kracht van CSS gegeven door oud student cmd Cyd Stumpel!<br
+      />
+      www.fdnd.nl<br />
+    </p>
+  </div>
+
+  <div class="container">
+    <h2>We web Navigeren op het web & de kracht van Css</h2>
+    <h3>Cyd Stumpel</h3>
+    <p>
+      2022-06-21T14:51:22.000Z <br />
+      Een talk over de kracht van CSS gegeven door oud student cmd Cyd Stumpel!<br
+      />
+      www.fdnd.nl<br />
+    </p>
+  </div>
+
+  <div class="container">
+    <h2>We web Navigeren op het web & de kracht van Css</h2>
+    <h3>Cyd Stumpel</h3>
+    <p>
+      2022-06-21T14:51:22.000Z <br />
+      Een talk over de kracht van CSS gegeven door oud student cmd Cyd Stumpel!<br
+      />
+      www.fdnd.nl<br />
+    </p>
+  </div>
 </section>
 
 <style>
   #sessions {
-    border: 1px #66e5bf solid;
-    border-radius: 2rem;
+    border: 3px solid #66e5bf;
+    border-radius: 2em;
+    height: 20em;
+    overflow-x: hidden;
+    padding-top: 20px;
+  }
+  .container {
+    width: 60em;
+    height: 10em;
+    border: solid 2px #a675f5;
+    border-radius: 2em;
+    margin: 2em;
+    padding: 1em;
   }
 </style>
--- src/lib/components/upcoming.svelte
+++ src/lib/components/upcoming.svelte
@@ -1,1 +1,162 @@
-<h3>Upcoming sessions</h3>
+<section id="upcomings">
+  <!-- upcoming we love web sessies -->
+  <section class="upcoming">
+    <div class="slider">
+      <h1>Upcoming We Love Web Sessies</h1>
+      <div class="slides">
+        <div id="slide-1">
+          <img src="./assets/IMG_7911.jpg" width="300em" />
+        </div>
+        <div id="slide-2">
+          <img src="./assets/IMG_7912.jpg" width="300em" />
+        </div>
+        <div id="slide-3">
+          <img src="./assets/IMG_7913.jpg" width="300em" />
+        </div>
+        <div id="slide-4">
+          <img src="./assets/IMG_7914.jpg" width="300em" />
+        </div>
+        <div id="slide-5">
+          <img src="./assets/IMG_7912.jpg" width="300em" />
+        </div>
+        <div id="slide-6">
+          <img src="./assets/IMG_7912.jpg" width="300em" />
+        </div>
+        <div id="slide-7">
+          <img src="./assets/IMG_7912.jpg" width="300em" />
+        </div>
+      </div>
+    </div>
+  </section>
+</section>
+
+<style>
+  #upcomings {
+    border: solid #66e5bf 2px;
+    border-radius: 2em;
+    /* margin: 2em; */
+    padding: 3em;
+  }
+  .slider h1 {
+    font-size: 3em;
+    /* padding-bottom: 3em;
+    padding-top: 2em; */
+  }
+
+  .upcoming {
+    background-color: #050542;
+    padding-bottom: 6em;
+  }
+
+  /* .slider {
+    border-top: #a675f5;
+    border-top-style: solid;
+    padding-right: 0;
+    width: 100%;
+    text-align: center;
+    overflow: hidden;
+    padding-top: 6em;
+  } */
+
+  /* image slider */
+
+  .slides img {
+    border-radius: 1em;
+  }
+  #slide-1:hover img {
+    opacity: 0.3;
+  }
+
+  #slide-2:hover img {
+    opacity: 0.3;
+  }
+
+  #slide-3:hover img {
+    opacity: 0.3;
+  }
+
+  #slide-4:hover img {
+    opacity: 0.3;
+  }
+
+  #slide-5:hover img {
+    opacity: 0.3;
+  }
+
+  .slides:hover #slide-1 {
+    opacity: 1;
+  }
+
+  .slides {
+    display: flex;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .slides::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+  }
+  .slides::-webkit-scrollbar-thumb {
+    background: #66e5bf;
+    border-radius: 10px;
+  }
+  .slides::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  .slides > div {
+    scroll-snap-align: start;
+    flex-shrink: 0;
+    width: 200px;
+    height: 200px;
+    margin-right: 120px;
+    border-radius: 10px;
+    background: #f1adee;
+    transform-origin: center center;
+    transform: scale(1);
+    transition: transform 0.5s;
+    position: relative;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 100px;
+  }
+
+  .author-info {
+    background: rgba(0, 0, 0, 0.75);
+    color: white;
+    padding: 0.75rem;
+    text-align: center;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    margin: 0;
+  }
+  .author-info a {
+    color: white;
+  }
+
+  .slider > a {
+    display: inline-flex;
+    width: 100%;
+    height: 1.5rem;
+    background: white;
+    text-decoration: none;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    margin: 0 0 0.5rem 0;
+    position: relative;
+    padding-right: 0;
+  }
+  .slider > a:active {
+    top: 1px;
+  }
+  .slider > a:focus {
+    background: #050542;
+  }
+</style>
